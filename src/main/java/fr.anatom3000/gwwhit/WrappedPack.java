d8113f727c0d422a57cf7f3597d4a0f83b97b77c
JFronny <33260128+jfronny@users.noreply.github.com>
2021-04-25 12:02:10 +0200

    Blyat

---
+++ src/main/java/fr.anatom3000/gwwhit/WrappedPack.java
@@ -0,0 +1,69 @@
+package fr.anatom3000.gwwhit;
+
+import net.minecraft.resource.ResourcePack;
+import net.minecraft.resource.ResourceType;
+import net.minecraft.resource.metadata.ResourceMetadataReader;
+import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.*;
+import java.util.function.Predicate;
+import java.util.Collection;
+import java.util.Set;
+
+public class WrappedPack implements ResourcePack {
+    private ResourcePack pack;
+    @Override
+    public InputStream openRoot(java.lang.String fileName) throws IOException {
+        if (fileName.endsWith(".ogg"))
+            return openBlyat();
+        return pack.openRoot(fileName);
+    }
+
+    @Override
+    public InputStream open(ResourceType type, Identifier id) throws IOException {
+        if (id.getPath().endsWith(".ogg"))
+            return openBlyat();
+        return pack.open(type, id);
+    }
+
+    @Override
+    public Collection<Identifier> findResources(ResourceType type, String namespace, String prefix, int maxDepth, Predicate<java.lang.String> pathFilter) {
+        return pack.findResources(type, namespace, prefix, maxDepth, pathFilter);
+    }
+
+    @Override
+    public boolean contains(ResourceType type, Identifier id) {
+        return pack.contains(type, id);
+    }
+
+    @Override
+    public Set<String> getNamespaces(ResourceType type) {
+        return pack.getNamespaces(type);
+    }
+
+    @Nullable
+    @Override
+    public <T> T parseMetadata(ResourceMetadataReader<T> metaReader) throws IOException {
+        return pack.parseMetadata(metaReader);
+    }
+
+    @Override
+    public String getName() {
+        return pack.getName();
+    }
+
+    @Override
+    public void close() {
+        pack.close();
+    }
+
+    public WrappedPack(ResourcePack pack) {
+        this.pack = pack;
+    }
+
+    private static InputStream openBlyat() {
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        return loader.getResourceAsStream("blyat.ogg");
+    }
+}

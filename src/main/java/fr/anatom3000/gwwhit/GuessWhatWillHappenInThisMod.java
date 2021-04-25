7d0d6669bd2f7d79de623db43e6eb47cac9654c9
Noaaan <noaaan@hotmail.com>
2021-04-26 00:43:18 +0200

    added dream RNG

--- src/main/java/fr/anatom3000/gwwhit/GuessWhatWillHappenInThisMod.java
+++ src/main/java/fr/anatom3000/gwwhit/GuessWhatWillHappenInThisMod.java
@@ -2,8 +2,15 @@ package fr.anatom3000.gwwhit;
 
 import fr.anatom3000.gwwhit.util.CachingTransformer;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
+import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.loot.UniformLootTableRange;
+import net.minecraft.loot.condition.RandomChanceLootCondition;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -16,8 +23,25 @@ public class GuessWhatWillHappenInThisMod implements ModInitializer {
 	public static final Random rng = new Random();
 	public static final CachingTransformer<Item> itemRandomizer = new CachingTransformer<>(s -> Registry.ITEM.getRandom(rng));
 	public static final UnaryOperator<ItemStack> itemStackRandomizer = s -> new ItemStack(itemRandomizer.apply(s.getItem()), s.getCount());
+	public static final Identifier LE_BLAZE_LOOT = new Identifier("minecraft","entities/blaze");
+
+	FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
+			.rolls(UniformLootTableRange.between(0,1))
+			.with(ItemEntry.builder(Items.BLAZE_ROD))
+			.withCondition(RandomChanceLootCondition.builder(0.38f).build());
+
 	@Override
 	public void onInitialize() {
 		System.out.println("Hello Fabric world!");
+		LootTableLoadingCallback.EVENT.register((resourceManager, manager, id, supplier, setter) -> {
+			if (LE_BLAZE_LOOT.equals(id)) {
+				supplier.withPool(poolBuilder.build());
+			}
+		});
 	}
 }
+
+
+
+
+

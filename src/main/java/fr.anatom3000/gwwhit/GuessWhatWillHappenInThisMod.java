7db410b16f32c7fc7f1c6129a16da0053abb46fe
anatom <49984698+anatom3000@users.noreply.github.com>
2021-04-25 10:19:41 +0200

    Update and rename src/main/java/net/fabricmc/example/ExampleMod.java to src/main/java/fr.anatom3000/gwwhit/GuessWhatWillHappenInThisMod.java

---
+++ src/main/java/fr.anatom3000/gwwhit/GuessWhatWillHappenInThisMod.java
@@ -0,0 +1,10 @@
+package fr.anatom3000.gwwhit.;
+
+import net.fabricmc.api.ModInitializer;
+
+public class GuessWhatWillHappenInThisMod implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		System.out.println("Hello Fabric world!");
+	}
+}

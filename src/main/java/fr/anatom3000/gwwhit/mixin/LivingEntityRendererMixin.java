478c08398c275674b71d8dc6b4ec0567c5b14376
minenash <minenash@protonmail.com>
2021-04-25 10:46:16 -0400

    Dinnerbone Entities

---
+++ src/main/java/fr/anatom3000/gwwhit/mixin/LivingEntityRendererMixin.java
@@ -0,0 +1,29 @@
+package fr.anatom3000.gwwhit.mixin;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.render.entity.LivingEntityRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.util.math.Vector3f;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Environment(EnvType.CLIENT)
+@Mixin(LivingEntityRenderer.class)
+public class LivingEntityRendererMixin {
+
+    @Inject(method = "setupTransforms", at = @At(value = "TAIL"))
+    private void dinnerboneEntities(LivingEntity entity, MatrixStack matrices, float _animationProgress, float _bodyYaw, float _tickDelta, CallbackInfo _info) {
+        if (!(entity instanceof PlayerEntity)) {
+            matrices.translate(0.0D, entity.getHeight() + 0.1F, 0.0D);
+            matrices.multiply(Vector3f.POSITIVE_Z.getDegreesQuaternion(180.0F));
+        }
+    }
+
+}

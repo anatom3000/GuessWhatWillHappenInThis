7540f8a739a91ecab5a5e1b09a731d497a05c0ee
Nikola Schrodinger <desuhansa@gmail.com>
2021-04-26 21:19:14 +0200

    Added trans-dimensional lens

--- src/main/java/fr/anatom3000/gwwhit/registry/ItemRegistry.java
+++ src/main/java/fr/anatom3000/gwwhit/registry/ItemRegistry.java
@@ -4,6 +4,7 @@ import fr.anatom3000.gwwhit.CustomItemGroups;
 import fr.anatom3000.gwwhit.GuessWhatWillHappenInThisMod;
 import fr.anatom3000.gwwhit.item.BoosterItem;
 import fr.anatom3000.gwwhit.item.PortableBlackHoleItem;
+import fr.anatom3000.gwwhit.item.TransdimensionalLensItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
@@ -17,6 +18,7 @@ public class ItemRegistry {
     private static final HashMap<String, Item> ITEMS = new HashMap<String, Item>() {{
         put("portable_black_hole", new PortableBlackHoleItem(new FabricItemSettings().fireproof().group(CustomItemGroups.GWWHITGroup)));
         put("booster", new BoosterItem(new FabricItemSettings().fireproof().group(CustomItemGroups.GWWHITGroup)));
+        put("transdimensional_lens", new TransdimensionalLensItem(new FabricItemSettings().fireproof().group(CustomItemGroups.GWWHITGroup)));
     }};
 
     public static void register() {

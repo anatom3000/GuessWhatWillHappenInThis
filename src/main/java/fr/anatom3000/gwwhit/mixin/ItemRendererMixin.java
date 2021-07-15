1d4fe0158dfacafe84b49fe5d686aa2f5203d9bf
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2021-07-15 23:55:09 +0200

    use java 16 in mixin config moved MinecraftClient mixin to client mixins

---
+++ src/main/java/fr/anatom3000/gwwhit/mixin/ItemRendererMixin.java
@@ -0,0 +1,8 @@
+package fr.anatom3000.gwwhit.mixin;
+
+import net.minecraft.client.render.item.ItemRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ItemRenderer.class)
+public class ItemRendererMixin {
+}

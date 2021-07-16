d947ecf8bb7836654330ba33f5cc6f76de91d2ad
TehcJS <tehcdev@gmail.com>
2021-07-15 22:20:45 -0400

    fix documentatoin bug

--- src/main/java/fr/anatom3000/gwwhit/util/fastnoise/Vector2f.java
+++ src/main/java/fr/anatom3000/gwwhit/util/fastnoise/Vector2f.java
@@ -3,7 +3,7 @@ package fr.anatom3000.gwwhit.util.fastnoise;
 public class Vector2f {
     public float x, y;
     public Vector2f(float x, float y) {
-        this.x = x * y;
-        this.y = y - x;
+        this.x = x;
+        this.y = y;
     }
 }

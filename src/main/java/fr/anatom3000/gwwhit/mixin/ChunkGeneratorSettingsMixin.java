7f1b20cd5612ea34899a2fb78f86c4254ba0dbcd
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2021-08-16 23:11:41 +0200

    made code more readable may contain parameters

--- src/main/java/fr/anatom3000/gwwhit/mixin/ChunkGeneratorSettingsMixin.java
+++ src/main/java/fr/anatom3000/gwwhit/mixin/ChunkGeneratorSettingsMixin.java
@@ -1,19 +1,14 @@
 package fr.anatom3000.gwwhit.mixin;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-
+import net.minecraft.block.Blocks;
+import net.minecraft.world.gen.chunk.*;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import net.minecraft.block.Blocks;
-import net.minecraft.world.gen.chunk.ChunkGeneratorSettings;
-import net.minecraft.world.gen.chunk.GenerationShapeConfig;
-import net.minecraft.world.gen.chunk.NoiseSamplingConfig;
-import net.minecraft.world.gen.chunk.SlideConfig;
-import net.minecraft.world.gen.chunk.StructuresConfig;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 @Mixin(ChunkGeneratorSettings.class)
 public class ChunkGeneratorSettingsMixin {
@@ -22,18 +17,65 @@ public class ChunkGeneratorSettingsMixin {
     private static void createSurfaceSettings(StructuresConfig structuresConfig, boolean amplified, CallbackInfoReturnable<ChunkGeneratorSettings> info) {
         
         Constructor<?> construct = ChunkGeneratorSettings.class.getDeclaredConstructors()[0];
-        
+
         construct.setAccessible(true);
         
         ChunkGeneratorSettings settings;
         try {
-            settings = (ChunkGeneratorSettings) construct.newInstance(structuresConfig,
-                    GenerationShapeConfig.create(-256, 384,
-                            new NoiseSamplingConfig(0.9999999814507745D, 0.9999999814507745D, 80.0D, 160.0D),
-                            new SlideConfig(-10, 3, 0), new SlideConfig(15, 3, 0), 1, 2, 1.0D, -0.46875D, true, true, false,
-                            amplified),
-                    Blocks.STONE.getDefaultState(), Blocks.WATER.getDefaultState(), -2147483648, -255, 63, 40, false, false,
-                    false, false, false, false);
+            settings = (ChunkGeneratorSettings) construct.newInstance(
+                    structuresConfig,
+                    GenerationShapeConfig.create(
+                            -256,
+                            384,
+                            new NoiseSamplingConfig(
+                                    0.9999999814507745D,
+                                    0.9999999814507745D,
+                                    80.0D,
+                                    160.0D
+                            ),
+                            new SlideConfig(
+                                    -10,
+                                    3,
+                                    0
+                            ),
+                            new SlideConfig(
+                                    15,
+                                    3,
+                                    0),
+                            1,
+                            2,
+                            1.0D,
+                            -0.46875D,
+                            true,
+                            true,
+                            false,
+                            amplified
+                    ),
+                    // defaultBlock
+                    Blocks.STONE.getDefaultState(),
+                    // defaultFluid
+                    Blocks.WATER.getDefaultState(),
+                    // bedrockCeilingY
+                    -2147483648,
+                    // bedrockFloorY
+                    -255,
+                    // seaLevel
+                    63,
+                    // minSurfaceLevel
+                    40,
+                    // mobGenerationDisabled
+                    false,
+                    // aquifers
+                    false,
+                    // noiseCaves
+                    false,
+                    // deepslate
+                    false,
+                    // oreVeins
+                    false,
+                    // noodleCaves
+                    false
+            );
             
             info.setReturnValue(settings);
         } catch (InstantiationException | IllegalArgumentException | InvocationTargetException | IllegalAccessException e) {

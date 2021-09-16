6be59df9278698d9bdd192a430da733aba401ba1
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2021-09-16 21:30:11 +0200

    stuff fork and more info buttons now exist

--- src/main/java/fr/anatom3000/gwwhit/gui/MainScreen.java
+++ src/main/java/fr/anatom3000/gwwhit/gui/MainScreen.java
@@ -2,6 +2,7 @@ package fr.anatom3000.gwwhit.gui;
 
 import fr.anatom3000.gwwhit.config.data.MainConfig;
 import fr.anatom3000.gwwhit.util.SafeUtils;
+import fr.anatom3000.gwwhit.util.Utilities;
 import me.shedaniel.autoconfig.AutoConfig;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
@@ -12,10 +13,12 @@ import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Util;
 
+import java.awt.*;
 import java.net.URI;
 
 public class MainScreen extends Screen {
-    private static final URI GITHUB = SafeUtils.doSafely(() -> new URI("https://www.youtube.com/watch?v=o-YBDTqX_ZU"));
+    private static final URI FORK = SafeUtils.doSafely(() -> new URI("https://github.com/anatom3000/GuessWhatWillHappenInThis/fork"));
+    private static final URI MORE_INFO = SafeUtils.doSafely(() -> new URI("https://www.youtube.com/watch?v=o-YBDTqX_ZU"));
 
     private final Screen parent;
 
@@ -26,9 +29,61 @@ public class MainScreen extends Screen {
 
     @Override
     protected void init() {
-        addDrawableChild(new ButtonWidget(this.width / 2 - 155, this.height / 6 + 48 - 6, 150, 20, new TranslatableText("gui.gwwhit.config"), (button) -> this.client.openScreen(AutoConfig.getConfigScreen(MainConfig.class, this).get())));
-        addDrawableChild(new ButtonWidget(this.width / 2 + 5, this.height / 6 + 48 - 6, 150, 20, new TranslatableText("gui.gwwhit.reload"), (button) -> MinecraftClient.getInstance().reloadResources()));
-        addDrawableChild(new ButtonWidget(this.width / 2 - 100, this.height / 6 + 168, 200, 20, ScreenTexts.DONE, (button) -> client.openScreen(this.parent)));
+        addDrawableChild(
+                new ButtonWidget(
+                        this.width / 2 - 155,
+                        this.height / 6 + 48 - 6,
+                        150,
+                        20,
+                        new TranslatableText("gui.gwwhit.config"),
+                        (button) -> MinecraftClient.getInstance().openScreen(
+                                    AutoConfig.getConfigScreen(
+                                            MainConfig.class,
+                                            this
+                                    ).get()
+                        )
+                )
+        );
+        addDrawableChild(
+                new ButtonWidget(
+                        this.width / 2 + 5,
+                        this.height / 6 + 48 - 6,
+                        150,
+                        20,
+                        new TranslatableText("gui.gwwhit.reload"),
+                        (button) -> MinecraftClient.getInstance().reloadResources()
+                )
+        );
+        addDrawableChild(
+                new ButtonWidget(
+                        this.width / 2 - 155,
+                        this.height / 6 + 70,
+                        150,
+                        20,
+                        new TranslatableText("gui.gwwhit.fork"),
+                        (button) -> Util.getOperatingSystem().open(FORK)
+                )
+        );
+        addDrawableChild(
+                new ButtonWidget(
+                        this.width / 2 + 5,
+                        this.height / 6 + 70,
+                        150,
+                        20,
+                        new TranslatableText("gui.gwwhit.more_info"),
+                        (button) -> Util.getOperatingSystem().open(MORE_INFO)
+                )
+        );
+        addDrawableChild(
+                new ButtonWidget(
+                        this.width / 2 - 100,
+                        this.height / 6 + 168,
+                        200,
+                        20,
+                        ScreenTexts.DONE,
+                        (button) -> MinecraftClient.getInstance().openScreen(this.parent)
+                )
+        );
     }
 
     @Override
--- src/main/java/fr/anatom3000/gwwhit/util/Utilities.java
+++ src/main/java/fr/anatom3000/gwwhit/util/Utilities.java
@@ -1,25 +1,40 @@
 package fr.anatom3000.gwwhit.util;
 
 import fr.anatom3000.gwwhit.commandline.ModUpdater;
+import net.minecraft.client.gui.screen.ConfirmChatLinkScreen;
 import org.jetbrains.annotations.NotNull;
 
+import java.awt.*;
+import java.io.IOException;
 import java.io.InputStream;
-import java.net.URISyntaxException;
+import java.net.URI;
 import java.net.URL;
 import java.nio.file.Path;
 
 /**
  * DONT USE ANY MC/MOD/MC-RELATED LIB CLASSES HERE
+ * safe to use class outside of mc
+ * @author ENDERZOMBI102
  */
 public final class Utilities {
     public static InputStream getResource(@NotNull String name) {
         return Utilities.class.getResourceAsStream(name);
     }
 
+    /**
+     * Given a resource name/path, returns its URL (includes path to jar file)
+     * @param name resource to search
+     */
     public static URL getResourceURL(@NotNull String name) {
         return Utilities.class.getResource(name);
     }
 
+    /**
+     * Checks if an array contains a value
+     * @param array array to check
+     * @param object obj to search for
+     * @return true if found
+     */
     public static <T> boolean contains(T[] array, @NotNull T object) {
         if (array == null)
             return false;
@@ -30,7 +45,12 @@ public final class Utilities {
         return false;
     }
 
-    public static Path getJarLocation() throws URISyntaxException {
-        return Path.of( ModUpdater.class.getProtectionDomain().getCodeSource().getLocation().toURI() );
+    /**
+     * Returns the location of the GWWHIT jar
+     * @return the path of the jar
+     */
+    @SuppressWarnings("ConstantConditions")
+    public static Path getJarLocation() {
+        return Path.of( SafeUtils.doSafely( () -> ModUpdater.class.getProtectionDomain().getCodeSource().getLocation().toURI() ) );
     }
 }
